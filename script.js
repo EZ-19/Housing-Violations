4327c83dd1b2e8c20a1c343919e3ab934ec02883
EZ-19 <111812260+EZ-19@users.noreply.github.com>
2022-11-22 16:11:05 -0500

    Added ChartJS with Default Info

--- script.js
+++ script.js
@@ -15,152 +15,222 @@ function getRandomIntInclusive(min, max) {
     const newMin = Math.ceil(min);
     const newMax = Math.floor(max);
     return Math.floor(Math.random() * (newMax - newMin + 1) + newMin); // The maximum is inclusive and the minimum is inclusive
-}
-
-function injectHTML(list) {
-  console.log('fired injectHTML');
-  const target = document.querySelector('#resturant_list');
-  target.innerHTML = '';
-
-  const listEl = document.createElement('ol');
-  target.appendChild(listEl);
-
-  list.forEach((item) => {
-    const el = document.createElement('li');
-    el.innerText = item.name;
-    listEl.appendChild(el);
-  });
-}
-
-function processRestaurants(list) {
-  console.log('fired restaurants list');
-  const range = [...Array(15).keys()]; // Special notation to create an array of 15 elements
-  const newArray = range.map((item) => {
-    const index = getRandomIntInclusive(0, list.length);
-    return list[index];
-  });
-  return newArray;
+  }
+  
+  function injectHTML(list) {
+    console.log('fired injectHTML');
+    const target = document.querySelector('#resturant_list');
+    target.innerHTML = '';
+  
+    const listEl = document.createElement('ol');
+    target.appendChild(listEl);
+  
+    list.forEach((item) => {
+      const el = document.createElement('li');
+      el.innerText = item.name;
+      listEl.appendChild(el);
+    });
+  }
+  
+  function processRestaurants(list) {
+    console.log('fired restaurants list');
+    const range = [...Array(15).keys()]; // Special notation to create an array of 15 elements
+    const newArray = range.map((item) => {
+      const index = getRandomIntInclusive(0, list.length);
+      return list[index];
+    });
+    return newArray;
+    /*
+        ## Process Data Separately From Injecting It
+          This function should accept your 1,000 records
+          then select 15 random records
+          and return an object containing only the restaurant's name, category, and geocoded location
+          So we can inject them using the HTML injection function
+  
+          You can find the column names by carefully looking at your single returned record
+          https://data.princegeorgescountymd.gov/Health/Food-Inspection/umjn-t2iz
+  
+        ## What to do in this function:
+  
+        - Create an array of 15 empty elements (there are a lot of fun ways to do this, and also very basic ways)
+        - using a .map function on that range,
+        - Make a list of 15 random restaurants from your list of 100 from your data request
+        - Return only their name, category, and location
+        - Return the new list of 15 restaurants so we can work on it separately in the HTML injector
+      */
+  }
+  
+  function filterList(array, filterInputValue) {
+    const newArray = array.filter((item) => {
+      const lowerCaseName = item.name.toLowerCase();
+      const lowerCaseQuery = filterInputValue.toLowerCase();
+      return lowerCaseName.includes(lowerCaseQuery);
+    });
+    return newArray;
+  }
+  
+  function initMap() {
+    console.log('init map');
+    const map = L.map('map').setView([38.9897, -76.9378], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    return map;
+  }
+  
+  function markerPlace(array, map) {
+    console.log('markerPlace', array);
+    map.eachLayer((layer) => {
+      if (layer instanceof L.Marker) {
+        layer.remove();
+      }
+    });
+  
+    array.forEach((item, index) => {
+      const {coordinates} = item.geocoded_column_1;
+      L.marker([coordinates[1], coordinates[0]]).addTo(map);
+      if (index === 0) {
+        map.setView([coordinates[1], coordinates[0]], 10);
+      }
+    });
+  }
+  
+  function initChart(chart, object) {
+    const labels = Object.keys(object);
+    const info = Object.keys(object).map((item) => object[item].length);
+  
+    const data = {
+      labels: labels,
+      datasets: [{
+        label: 'Restaurants By Category',
+        backgroundColor: 'rgb(255, 99, 132)',
+        bordercolor: 'rgb(255, 99, 132)',
+        data: info
+      }]
+    };
+  
+    const config = {
+      type: 'line',
+      data: data,
+      options: {}
+    };
+  
+    return new Chart(
+      chart,
+      config
+    );
+  }
+  
+  function changeChart(chart, dataObject) {
+    const labels = Object.keys(dataObject);
+    const info = Object.keys(dataObject).map((item) => dataObject[item].length);
+  
+    chart.data.labels = labels;
+    chart.data.datasets.forEach((set) => {
+      set.data = info;
+      return set;
+    });
+  
+    chart.update();
+  }
+  
+  function shapeDataForLineChart(array) {
+    return array.reduce((collection, item) => {
+      if (!collection[item.category]) {
+        collection[item.category] = [item];
+      } else {
+        collection[item.category].push(item);
+      }
+      return collection;
+    }, {});
+  }
+  
+  async function getData() {
+    const url = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'; // remote URL! you can test it in your browser
+    const data = await fetch(url); // We're using a library that mimics a browser 'fetch' for simplicity
+    const json = await data.json(); // the data isn't json until we access it using dot notation
+    const reply = json.filter((item) => Boolean(item.geocoded_column_1)).filter((item) => Boolean(item.name));
+    return reply;
+  }
+  
+  async function mainEvent() {
+    // const pageMap = initMap();
+  
+    // the async keyword means we can make API requests
+    const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
+    const submit = document.querySelector('#get-resto'); // get a reference to your submit button
+    const loadAnimation = document.querySelector('.lds-ellipsis');
+    const chartTarget = document.querySelector('#myChart');
+    submit.style.display = 'none'; // let your submit button disappear
+  
+    /*
+        Let's get some data from the API - it will take a second or two to load
+        This next line goes to the request for 'GET' in the file at /server/routes/foodServiceRoutes.js
+        It's at about line 27 - go have a look and see what we're retrieving and sending back.
+       */
+    // const results = await fetch('/api/foodServicePG');
+    // const arrayFromJson = await results.json(); // here is where we get the data from our request as JSON
+  
+    // API Data Request
+    const chartData = await getData();
+    const shapedData = shapeDataForLineChart(chartData);
+    console.log(shapedData);
+    const myChart = initChart(chartTarget, shapedData);
+  
+    // console.log(arrayFromJson.data[0]);
+  
+    // this is called "string interpolation" and is how we build large text blocks with variables
+    // console.log(`${arrayFromJson.data[0].name} ${arrayFromJson.data[0].category}`);
+  
+    // This IF statement ensures we can't do anything if we don't have information yet
+    // if (!arrayFromJson.data?.length) { return; } // Return if we have no data
+  
+    let currentList = [];
+  
+    // let's turn the submit button back on by setting it to display as a block when we have data available
+    submit.style.display = 'block';
+  
+    // Hidding the load button
+    loadAnimation.classList.remove('lds-ellipsis');
+    loadAnimation.classList.add('lds-ellipsis_hidden');
+  
+    form.addEventListener('input', (event) => {
+      console.log('input', event.target.value);
+      const newFilterList = filterList(currentList, event.target.value);
+      injectHTML(newFilterList);
+      const localData = shapeDataForLineChart(newFilterList);
+      changeChart(myChart, localData);
+      console.log(localData);
+      // markerPlace(newFilterList, pageMap);
+    });
+  
+    // And here's an eventListener! It's listening for a "submit" button specifically being clicked
+    // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
+    form.addEventListener('submit', (submitEvent) => {
+      // This is needed to stop our page from changing to a new URL even though it heard a GET request
+      submitEvent.preventDefault();
+  
+      // This constant will have the value of your 15-restaurant collection when it processes
+      currentList = processRestaurants(chartData);
+      console.log(currentList);
+  
+      // And this function call will perform the "side effect" of injecting the HTML list for you
+      injectHTML(currentList);
+      const localData = shapeDataForLineChart(currentList);
+      changeChart(myChart, localData);
+      console.log(localData);
+      // markerPlace(currentList, pageMap);
+      // By separating the functions, we open the possibility of regenerating the list
+      // without having to retrieve fresh data every time
+      // We also have access to some form values, so we could filter the list based on name
+    });
+  }
+  
   /*
-      ## Process Data Separately From Injecting It
-        This function should accept your 1,000 records
-        then select 15 random records
-        and return an object containing only the restaurant's name, category, and geocoded location
-        So we can inject them using the HTML injection function
-
-        You can find the column names by carefully looking at your single returned record
-        https://data.princegeorgescountymd.gov/Health/Food-Inspection/umjn-t2iz
-
-      ## What to do in this function:
-
-      - Create an array of 15 empty elements (there are a lot of fun ways to do this, and also very basic ways)
-      - using a .map function on that range,
-      - Make a list of 15 random restaurants from your list of 100 from your data request
-      - Return only their name, category, and location
-      - Return the new list of 15 restaurants so we can work on it separately in the HTML injector
+      This last line actually runs first!
+      It's calling the 'mainEvent' function at line 57
+      It runs first because the listener is set to when your HTML content has loaded
     */
-}
-
-function filterList(array, filterInputValue) {
-  const newArray = array.filter((item) => {
-    const lowerCaseName = item.name.toLowerCase();
-    const lowerCaseQuery = filterInputValue.toLowerCase();
-    return lowerCaseName.includes(lowerCaseQuery);
-  });
-  return newArray;
-}
-
-function initMap() {
-  console.log('init map');
-  const map = L.map('map').setView([38.9897, -76.9378], 13);
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  }).addTo(map);
-  return map;
-}
-
-function markerPlace(array, map) {
-  console.log('markerPlace', array);
-  map.eachLayer((layer) => {
-    if (layer instanceof L.Marker) {
-      layer.remove();
-    }
-  });
-
-  array.forEach((item, index) => {
-    const {coordinates} = item.geocoded_column_1;
-    L.marker([coordinates[1], coordinates[0]]).addTo(map);
-    if (index === 0) {
-      map.setView([coordinates[1], coordinates[0]], 10);
-    }
-  });
-}
-
-async function mainEvent() {
-
-  const pageMap = initMap();
-
-  // the async keyword means we can make API requests
-  const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
-  const submit = document.querySelector('#get-resto'); // get a reference to your submit button
-  const loadAnimation = document.querySelector('.lds-ellipsis');
-  submit.style.display = 'none'; // let your submit button disappear
-
-  /*
-      Let's get some data from the API - it will take a second or two to load
-      This next line goes to the request for 'GET' in the file at /server/routes/foodServiceRoutes.js
-      It's at about line 27 - go have a look and see what we're retrieving and sending back.
-     */
-  const results = await fetch('/api/foodServicePG');
-  const arrayFromJson = await results.json(); // here is where we get the data from our request as JSON
-
-  console.log(arrayFromJson.data[0]);
-
-  // this is called "string interpolation" and is how we build large text blocks with variables
-  console.log(`${arrayFromJson.data[0].name} ${arrayFromJson.data[0].category}`);
-
-  // This IF statement ensures we can't do anything if we don't have information yet
-  if (!arrayFromJson.data?.length) { return; } // Return if we have no data
-
-  let currentList = [];
-
-  // let's turn the submit button back on by setting it to display as a block when we have data available
-  submit.style.display = 'block'; 
-
-  // Hidding the load button
-  loadAnimation.classList.remove('lds-ellipsis');
-  loadAnimation.classList.add('lds-ellipsis_hidden');
-
-  form.addEventListener('input', (event) => {
-    console.log('input', event.target.value);
-    const newFilterList = filterList(currentList, event.target.value);
-    injectHTML(newFilterList);
-    markerPlace(newFilterList, pageMap);
-  });
-
-  // And here's an eventListener! It's listening for a "submit" button specifically being clicked
-  // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
-  form.addEventListener('submit', (submitEvent) => {
-    // This is needed to stop our page from changing to a new URL even though it heard a GET request
-    submitEvent.preventDefault();
-
-    // This constant will have the value of your 15-restaurant collection when it processes
-    currentList = processRestaurants(arrayFromJson.data);
-    console.log(currentList);
-
-    // And this function call will perform the "side effect" of injecting the HTML list for you
-    injectHTML(currentList);
-    markerPlace(currentList, pageMap);
-    // By separating the functions, we open the possibility of regenerating the list
-    // without having to retrieve fresh data every time
-    // We also have access to some form values, so we could filter the list based on name
-  });
-}
-
-/*
-    This last line actually runs first!
-    It's calling the 'mainEvent' function at line 57
-    It runs first because the listener is set to when your HTML content has loaded
-  */
-document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
+  document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
   
